eb0b3df2b572c8380da0c297280abd44d485b721
vinisant0lopes-crypto <vinisant0lopes@gmail.com>
2026-09-23 16:39:04 -0300

    concretizando o sistema de criação de cards

--- src/scripts/criar_cartoes.js
+++ src/scripts/criar_cartoes.js
@@ -5,14 +5,20 @@ function criar_cartoes(card, container_cards) {
 
 
 // numero de cartões a ser adicionado
-const n_cartoes = 20;
+const n_cartoes = 9;
 
 // laço de repetição
 for (let i = 0; i < n_cartoes; i++) {
-    const divDuplicada = card.cloneNode(true)
-
+    const divDuplicada = card.cloneNode(true);
+    
+    
+    divDuplicada.removeAttribute('id');
+    divDuplicada.style.display = 'block';
+    
+    
     // aplicar as divs duplicadas para o container
     container_cards.appendChild(divDuplicada)
+    
 }
 
 };
